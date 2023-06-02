34bd9210d2eb0011b1010635db6e8d8f88824a97
Nikolai Matijevic <61649682+nikolai-matijevic@users.noreply.github.com>
2023-06-02 10:06:55 +0200

    refactor: update files to reflect mod name

---
+++ src/main/java/com/zimtrax/logstochest/LogsToChest.java
@@ -0,0 +1,35 @@
+package com.zimtrax.logstochest;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(LogsToChest.MODID)
+public class LogsToChest
+{
+    public static final String MODID = "logstochest";
+
+    public LogsToChest()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){}
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){}
+    }
+}
